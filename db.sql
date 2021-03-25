1945c4975db1eb42e6bff004eee0a7423e5f721c
Faber Ordoñez <faberordonez1@gmail.com>
2021-03-24 23:57:15 -0500

    conexion db

---
+++ db.sql
@@ -0,0 +1,15 @@
+create table areas(
+    id int(11) auto_increment primary key,
+    nombre varchar(255) not null
+);
+
+ create table empleados(
+     id int(11) auto_increment primary key,
+     nombre varchar(255) not null,
+     email varchar(255) not null,
+     sexo char(1) not null,
+     area_id int(11) not null,
+     boletin int(11) not null,
+     descripcion text not null,
+     foreign key (area_id) references areas(id)
+);
